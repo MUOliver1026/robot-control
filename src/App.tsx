7077ae4d776c3313a7e08ef04314b8c2a6d0d503
Oliver Wu <z5379238@ad.unsw.edu.au>
2024-08-07 16:51:42 +1000

    fix deploying bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Grid from './components/Grid';
 import { RootState, moveForward, rotate } from './store';
